32ff9e6aeab6074ff12a6f6c2d21232b31caabda
noahx0001 <lopeznoe191119@gmail.com>
2025-03-17 00:50:03 -0600

    Comentarios para la clase.

--- src/app/echo-config.ts
+++ src/app/echo-config.ts
@@ -1,8 +1,10 @@
 import Pusher from 'pusher-js';
 import Echo from 'laravel-echo';
 
+// Guardar la instancia de Pusher en la ventana global para que pueda ser accedida desde cualquier lugar
 (window as any).Pusher = Pusher;
 
+// Declaramos la instancia de Echo y la exportamos para usarla en otros componentes
 export const echo = new Echo({
     broadcaster: 'pusher',
     key: '4a99c14c8cdf7d757b7c',
